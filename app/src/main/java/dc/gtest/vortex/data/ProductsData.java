69d0f25c6b7bfc4bc86119cd0b699a24f60a03dd
Kimon <kzeliotis@dataconsulting.gr>
2025-01-21 16:59:46 +0200

    6.04.02 product components (In progress)

--- app/src/main/java/dc/gtest/vortex/data/ProductsData.java
+++ app/src/main/java/dc/gtest/vortex/data/ProductsData.java
@@ -56,7 +56,7 @@ public class ProductsData {
                     productModel.setProjectInstallationId(MyJsonParser.getStringValue(oneObjectProduct, "InstallationId", "0"));
                     productModel.setMasterId(MyJsonParser.getStringValue(oneObjectProduct, "MasterId", "0"));
                     productModel.setNotes(MyJsonParser.getStringValue(oneObjectProduct, "notes", ""));
-                    productModel.setMasterProductComponentId(MyJsonParser.getStringValue(oneObjectProduct, "MasterProductComponentId", "0"));
+                    productModel.setProductComponentId(MyJsonParser.getStringValue(oneObjectProduct, "ProductComponentId", "0"));
 
                     JSONArray jArrayProductAttributes = new JSONArray();
                     JSONArray jArrayMandatoryAttributes = new JSONArray();
